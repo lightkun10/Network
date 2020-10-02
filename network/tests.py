1c757eb121ccd19bd37fd38124d107f395fa6eb2
lightkun10 <lifeisubw@gmail.com>
2020-10-02 09:56:10 +0700

    Add initial test for follow model

--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from .models import User, Follow
+
+
 # Create your tests here.
+class ModelsTestCase(TestCase):
+
+    def setUp(self):
+        return super().setUp()
