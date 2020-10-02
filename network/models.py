53c8fff7182d0a8eca1f99e08dfdd6358fa14c42
lightkun10 <lifeisubw@gmail.com>
2020-10-02 09:24:53 +0700

    Add follow model

--- network/models.py
+++ network/models.py
@@ -6,7 +6,10 @@ class User(AbstractUser):
     # following, followers, posts, likes?
     pass
 
-
+class Follow(models.Model):
+    user = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
+    user_follow = models.ForeignKey(User, related_name="user_follow", on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
 
 """
 You will also need to add additional models 
