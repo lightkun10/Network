bf6ae80c236bb10d195228e760f71e7e082cf708
lightkun10 <lifeisubw@gmail.com>
2020-10-03 12:04:11 +0700

    Feature for adding new post

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"), # default route
     path("login", views.login_view, name="login"), # login route
     path("logout", views.logout_view, name="logout"), # logout route
-    path("register", views.register, name="register") # register route
+    path("register", views.register, name="register"), # register route,
+
+    # API routes
+    path("post", views.create, name="create")
 ]
