8356f6d29715a7f180a4c03e3c7d816d285e42a8
lightkun10 <lifeisubw@gmail.com>
2020-10-03 12:12:08 +0700

    Add new post submit feature

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -2,5 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     /** For JS on index page/all post page */
+    document.querySelector('#create-form').onsubmit = function() {
+        let text = document.querySelector('#post-text').value;
+        console.log(`You are typing: ${text}`);
 
+        return false;
+    }
 })
