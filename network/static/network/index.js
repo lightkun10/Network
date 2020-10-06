2de219b2c6d7564b41dc00b9fc7d6b2f60f049ac
lightkun10 <lifeisubw@gmail.com>
2020-10-06 16:03:10 +0700

    Display all posts at index route.

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -31,7 +31,65 @@ function load_posts() {
     fetch(`/posts`)
     .then(response => response.json())
     .then(posts => {
-        console.log(posts);
+        //console.log(posts);
+
+        // for every posts
+        posts.forEach(post => {
+            let id = post["id"];
+            let username = post["username"];
+            let content = post["content"];
+            let created_at = post["created_at"];
+            let likes = post["likes"];
+
+            // Make a new div to display post
+            const p = document.createElement('div');
+            p.className = `post post-${id}`
+
+            const postUser = document.createElement('div');
+            postUser.className = 'post-user';
+            postUser.innerHTML = `<h4>${username}</h4>`
+            p.append(postUser);
+
+            let editButton = document.createElement('button');
+            editButton.className = 'post-editBtn btn btn-info btn-sm';
+            editButton.innerHTML = 'Edit';
+            p.append(editButton);
+            editButton.addEventListener('click', function() {
+                console.log("Edit post content, but for now I just print the log here...");
+            })
+            
+            const postContent = document.createElement('div');
+            postContent.className = 'post-content';
+            postContent.innerHTML = `${content}`
+            p.append(postContent);
+
+            const postTime = document.createElement('div');
+            postTime.className = 'post-time';
+            postTime.innerHTML = created_at;
+            p.append(postTime);
+
+            const postLikes = document.createElement('div');
+            postLikes.className = 'post-likes';
+            let likeLogo = document.createElement("img");
+            likeLogo.src = 'static/images/unlike.png';
+            likeLogo.setAttribute("height", "18.75");
+            likeLogo.setAttribute("width", "18.75");
+            const likesCounts = document.createElement("span");
+            likesCounts.className = 'post-likes-counts'
+            likesCounts.textContent = ` ${likes}`;
+            postLikes.appendChild(likeLogo);
+            postLikes.appendChild(likesCounts)
+            p.append(postLikes);
+            postLikes.addEventListener('click', function() {
+                console.log("Increment like counts and change the logo");
+            })
+
+            // Append username to the all post section
+            document.querySelector('.all-post-section').append(p);
+
+
+            
+        });
     });
 
 }
